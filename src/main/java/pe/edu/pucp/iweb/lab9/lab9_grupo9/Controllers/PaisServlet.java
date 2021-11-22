fabe74826872e3eb45b4c713d29c8b8903be58de
Javier Marroquin <a20193644@pucp.edu.pe>
2021-11-22 18:51:27 -0500

    Servlets

---
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/PaisServlet.java
@@ -0,0 +1,19 @@
+package pe.edu.pucp.iweb.lab9.lab9_grupo9.Controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PaisServlet", value = "/PaisServlet")
+public class PaisServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/ParticipanteServlet.java
@@ -0,0 +1,19 @@
+package pe.edu.pucp.iweb.lab9.lab9_grupo9.Controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ParticipanteServlet", value = "/ParticipanteServlet")
+public class ParticipanteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
