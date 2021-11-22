cebf5d7b3df3915356c5319abd01b76a7d8c0e07
Javier Marroquin <a20193644@pucp.edu.pe>
2021-11-22 18:58:07 -0500

    Daos

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/ParticipanteDao.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/ParticipanteDao.java
@@ -1,4 +1,5 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos;
 
 public class ParticipanteDao extends BaseDao{
+
 }
--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/UniversidadDao.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/UniversidadDao.java
@@ -1,4 +1,6 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos;
 
 public class UniversidadDao extends BaseDao{
+
+
 }
