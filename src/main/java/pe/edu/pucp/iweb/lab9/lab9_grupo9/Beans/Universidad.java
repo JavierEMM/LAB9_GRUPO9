1c984acce8a8a9b7d77bcc119bd3544d36ea2cd5
Alonso Rosales <a20191566@pucp.edu.pe>
2021-11-22 18:56:56 -0500

    Mas beans

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Beans/Universidad.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Beans/Universidad.java
@@ -1,5 +1,52 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans;
 
 public class Universidad {
+    private String nombre;
+    private String pais;
+    private String ranking;
+    private int numeroAlumnos;
+    private String foto;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(String ranking) {
+        this.ranking = ranking;
+    }
+
+    public int getNumeroAlumnos() {
+        return numeroAlumnos;
+    }
+
+    public void setNumeroAlumnos(int numeroAlumnos) {
+        this.numeroAlumnos = numeroAlumnos;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+
 
 }
