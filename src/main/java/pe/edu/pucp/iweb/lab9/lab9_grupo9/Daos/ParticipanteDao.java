624639c1e2f4593aa1fae193172181a09442ad2e
Alonso Rosales <a20191566@pucp.edu.pe>
2021-11-22 19:53:17 -0500

    dao

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/ParticipanteDao.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/ParticipanteDao.java
@@ -2,22 +2,27 @@ package pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos;
 
 import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Participante;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ParticipanteDao extends BaseDao{
 
     public ArrayList<Participante> listaParticipantes(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
         ArrayList<Participante> listita = new ArrayList<>();
 
         String sentenciaSQL = "SELECT participante.nombre,apellidos,edad,genero,p.nombre FROM participante\n"+
                                 "INNER JOIN pais p ON (participante.pais_idpais = p.idpais);";
 
+        String url = "jdbc:mysql://localhost:3306/lab9?serverTimezone=America/Lima";
 
-        try (Connection conn = this.getConnection();
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sentenciaSQL)) {
 
