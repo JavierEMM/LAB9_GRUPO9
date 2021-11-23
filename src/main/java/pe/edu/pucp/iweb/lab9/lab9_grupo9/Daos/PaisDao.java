d7a69b09203344a2a7d881ce950b3eb85f9dbead
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2021-11-22 19:53:01 -0500

    listar Paises filtrados

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/PaisDao.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/PaisDao.java
@@ -2,10 +2,7 @@ package pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos;
 
 import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Pais;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class PaisDao extends BaseDao{
@@ -14,10 +11,29 @@ public class PaisDao extends BaseDao{
 
         try(Connection conn = this.getConnection();
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT p.nombre, c.nombre as continente , p.tamanho, p.poblacion FROM lab9.pais p\n" +
-                    "inner join\tlab9.continente c on c.idcontinente = p.continente_idcontinente")){
+            ResultSet rs = stmt.executeQuery("SELECT p.idpais ,p.nombre, c.nombre as continente , p.tamanho, p.poblacion FROM lab9.pais p\n" +
+                    "inner join\tlab9.continente c on c.idcontinente = p.continente_idcontinente order by p.nombre")){
             while (rs.next()){
-                Pais pais = new Pais(rs.getString(1),rs.getString(2),rs.getString(3),rs.getInt(4));
+                Pais pais = new Pais(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5));
+                listaPaises.add(pais);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return listaPaises;
+    }
+    public ArrayList<Pais> listarPaisesFiltrados(int idcontinente){
+        ArrayList<Pais> listaPaises = new ArrayList<>();
+        String sql = "SELECT p.idpais ,p.nombre, c.nombre as continente , p.tamanho, p.poblacion FROM lab9.pais p\n" +
+                "inner join\tlab9.continente c on c.idcontinente = p.continente_idcontinente where c.idcontinente = ? order by p.nombre";
+
+        try(Connection conn = this.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ){
+            pstmt.setInt(1,idcontinente);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()){
+                Pais pais = new Pais(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5));
                 listaPaises.add(pais);
             }
         }catch (SQLException e){
