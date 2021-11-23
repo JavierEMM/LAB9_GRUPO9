96aa12403c442986e3942ee413fc1f2874c79ff0
Alonso Rosales <a20191566@pucp.edu.pe>
2021-11-22 19:06:33 -0500

    in

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/MainServlet.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/MainServlet.java
@@ -1,6 +1,7 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Controllers;
 
 import java.io.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -8,11 +9,9 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "")
 public class MainServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-
-
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+        view.forward(request, response);
 
     }
 
