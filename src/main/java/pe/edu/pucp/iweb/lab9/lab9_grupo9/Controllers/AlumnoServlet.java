521c86c6fc9db970999c8a27be47f0d5d2ca981d
Alonso Rosales <a20191566@pucp.edu.pe>
2021-11-23 22:01:37 -0500

    Universidad-Alumno terminado

--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/AlumnoServlet.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Controllers/AlumnoServlet.java
@@ -1,5 +1,8 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Controllers;
 
+import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Alumno;
+import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Participante;
+import pe.edu.pucp.iweb.lab9.lab9_grupo9.DTO.ParticipanteDTO;
 import pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos.AlumnoDao;
 import pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos.ParticipanteDao;
 
@@ -7,34 +10,44 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "AlumnoServlet", value = "/AlumnoServlet")
 public class AlumnoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String accion = request.getParameter("action") == null ?  "mostrar": request.getParameter("action");
+        String accion = request.getParameter("action") != null ?  request.getParameter("action") : "mostrar";
         RequestDispatcher view;
         AlumnoDao alumnoDao = new AlumnoDao();
         ParticipanteDao participanteDao = new ParticipanteDao();
-        switch (accion){
+        switch (accion) {
             case "mostrar":
                 String idUniversidadStr = request.getParameter("id");
                 int idUniversidad1 = Integer.parseInt(idUniversidadStr);
-                request.setAttribute("listaAlumnosPorUniversidad",alumnoDao.listaAlumnos(idUniversidad1));
-                request.setAttribute("vista","mostrar");
-                request.setAttribute("idUniversidad",idUniversidadStr);
+                request.setAttribute("listaAlumnosPorUniversidad", alumnoDao.listaAlumnos(idUniversidad1));
+                request.setAttribute("vista", "mostrar");
+                request.setAttribute("idUniversidad", idUniversidadStr);
                 view = request.getRequestDispatcher("/alumnos.jsp");
                 view.forward(request, response);
                 break;
             case "editar":
-
+                String idParticipanteStr = request.getParameter("id");
+                int idParticipante = Integer.parseInt(idParticipanteStr);
+                Alumno alumnito = alumnoDao.obtenerAlumnoPorId(idParticipante);
+                request.setAttribute("Alumno",alumnito);
+                request.setAttribute("vista","editar");
+                view = request.getRequestDispatcher("/editaralumnos.jsp");
+                view.forward(request, response);
                 break;
             case "crear":
-
-
-
-
-
+                String idUniversidadStr1 = request.getParameter("idUniversidad");
+                ArrayList<Participante> listaParticipanteNoUniversidad = new ArrayList<>();
+                listaParticipanteNoUniversidad = alumnoDao.listaParticipantesCrear(Integer.parseInt(idUniversidadStr1));
+                request.setAttribute("listaParticipantesNoUniversidad",listaParticipanteNoUniversidad);
+                request.setAttribute("vista","crear");
+                request.setAttribute("idUniversidad",idUniversidadStr1);
+                view = request.getRequestDispatcher("/editaralumnos.jsp");
+                view.forward(request, response);
                 break;
             case "borrar":
                 String idAlumnoEditar = request.getParameter("id");
@@ -66,6 +79,44 @@ public class AlumnoServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String accion = request.getParameter("action");
 
+        ParticipanteDao participanteDao = new ParticipanteDao();
+        switch (accion) {
+            case "crear":
+                String idUniversidadStr = request.getParameter("idUniversidad");
+                int idUniversidad = Integer.parseInt(idUniversidadStr);
+                String idParticipanteStr = request.getParameter("idParticipante");
+                int idParticipante = Integer.parseInt(idParticipanteStr);
+                String codigoParticipanteStr = request.getParameter("codigoParticipante");
+                int codigoParticipante = Integer.parseInt(codigoParticipanteStr);
+                String promedioParticipanteStr = request.getParameter("promedioParticipante");
+                double promedioParticipante = Double.parseDouble(promedioParticipanteStr);
+                AlumnoDao alumnoDao = new AlumnoDao();
+                alumnoDao.insertAlumno(idParticipante,codigoParticipante,promedioParticipante,idUniversidad);
+                response.sendRedirect(request.getContextPath()+ "/UniversidadServlet");
+                break;
+
+            case "editar":
+                String idAlumnoStr = request.getParameter("id");
+                int idAlumno = Integer.parseInt(idAlumnoStr);
+                String codigoAlumnoStr = request.getParameter("codigoAlumno");
+                int codigoAlumno = Integer.parseInt(codigoAlumnoStr);
+                String nombreAlumno = request.getParameter("nombreAlumno");
+                String apellidoAlumno = request.getParameter("apellidoAlumno");
+                String edadAlumnoStr = request.getParameter("edadAlumno");
+                int edadAlumno = Integer.parseInt(edadAlumnoStr);
+                String condicionAlumno = request.getParameter("condicionAlumno");
+                String promedioAlumnoStr = request.getParameter("promedioAlumno");
+                double promedioAlumno = Double.parseDouble(promedioAlumnoStr);
+
+                Alumno alumnito = new Alumno(nombreAlumno,apellidoAlumno,edadAlumno,idAlumno,codigoAlumno,promedioAlumno,condicionAlumno);
+                AlumnoDao alumnoDao1 = new AlumnoDao();
+                alumnoDao1.updateAlumno(alumnito);
+                response.sendRedirect(request.getContextPath()+ "/UniversidadServlet");
+                break;
+
+
+        }
     }
 }
--- src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/AlumnoDao.java
+++ src/main/java/pe/edu/pucp/iweb/lab9/lab9_grupo9/Daos/AlumnoDao.java
@@ -1,11 +1,10 @@
 package pe.edu.pucp.iweb.lab9.lab9_grupo9.Daos;
 
 import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Alumno;
+import pe.edu.pucp.iweb.lab9.lab9_grupo9.Beans.Participante;
+import pe.edu.pucp.iweb.lab9.lab9_grupo9.DTO.ParticipanteDTO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class AlumnoDao extends  BaseDao {
@@ -73,4 +72,125 @@ public class AlumnoDao extends  BaseDao {
         }
     }
 
+    public ArrayList<Participante> listaParticipantesCrear (int idUniversidad){
+        ArrayList<Participante> listita = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String sentenciaSQL = "SELECT participante.idparticipante,participante.nombre FROM participante\n"+
+                                "LEFT JOIN alumno ON (participante.idparticipante = alumno.participante_idparticipante)\n"+
+                                "LEFT JOIN universidad ON (alumno.universidad_iduniversidad = universidad.iduniversidad)\n"+
+                                "WHERE codigo IS NULL AND participante.pais_idpais = ?;";
+
+        try (Connection conn = this.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sentenciaSQL)) {
+            pstmt.setInt(1,idUniversidad);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Participante participante = new Participante();
+                participante.setIdParticipante(rs.getInt(1));
+                participante.setNombre(rs.getString(2));
+                listita.add(participante);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return listita;
+    }
+
+    public  void insertAlumno(int idParticipante, int codigoParticipante, double promedioParticipante,int idUniversidad) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String sentenciaSQL = "INSERT INTO alumno (codigo,promedio_ponderado,condicion,participante_idparticipante,universidad_iduniversidad)\n"+
+                                "VALUES (?,?,?,?,?);";
+
+        try (Connection conn = this.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sentenciaSQL)) {
+
+            pstmt.setInt(1,codigoParticipante);
+            pstmt.setDouble(2,promedioParticipante);
+            pstmt.setString(3,"Normal");
+            pstmt.setInt(4,idParticipante);
+            pstmt.setInt(5,idUniversidad);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public Alumno obtenerAlumnoPorId(int idParticipante){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Alumno alumno = null;
+        String sentenciaSQL =   "SELECT p.nombre,p.apellidos,p.edad,a.codigo,a.promedio_ponderado,a.condicion FROM alumno a\n"+
+                                "INNER JOIN participante p ON (a.participante_idparticipante = p.idparticipante)\n"+
+                                "WHERE a.participante_idparticipante = ?";
+
+        try (Connection conn = this.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sentenciaSQL);){
+            pstmt.setInt(1, idParticipante);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                //SIEMPRE HAY UN PARTICIPANTE
+                String nombre = rs.getString(1);
+                String apellido = rs.getString(2);
+                int edad = rs.getInt(3);
+                int codigo = rs.getInt(4);
+                double promedio = rs.getDouble(5);
+                String condicion = rs.getString(6);
+                alumno = new Alumno(nombre,apellido,edad,idParticipante,codigo,promedio,condicion);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return alumno;
+    }
+
+
+    public  void updateAlumno(Alumno alumno) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String sentenciaSQL = "UPDATE participante \n" +
+                "INNER JOIN alumno a ON (participante.idparticipante = a.participante_idparticipante)\n" +
+                "SET nombre = ?,apellidos = ? , edad = ? , a.codigo = ?,a.promedio_ponderado = ?,a.condicion = ? WHERE a.participante_idparticipante = ?";
+
+        try (Connection conn = this.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sentenciaSQL)) {
+
+            pstmt.setString(1,alumno.getNombre());
+            System.out.println(alumno.getNombre());
+            pstmt.setString(2,alumno.getApellido());
+            pstmt.setInt(3,alumno.getEdad());
+            pstmt.setInt(4,alumno.getCodigo());
+            pstmt.setDouble(5,alumno.getPromedioPonderado());
+            pstmt.setString(6,alumno.getCondicion());
+            pstmt.setInt(7,alumno.getIdParticipante());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
